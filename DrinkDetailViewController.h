b269480b9ef4982aa8b522943d19233caa5270d0
Christian Herlambang <christian.herlambang@gmail.com>
2011-07-01 12:28:49 +0200

    start adding detail view

---
+++ DrinkDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DrinkDetailViewController.h
+//  DrinkMixer
+//
+//  Created by Christian Phang on 7/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DrinkDetailViewController : UIViewController {
+
+}
+
+@end
